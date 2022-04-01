50cc6c435dc660586ec2a4e012d94c996cd05195
luckyh15 <95lovekyh@naver.com>
2022-04-01 13:16:07 +0900

    [용환]왁자지껄 구축

---
+++ src/main/java/com/semi/dangjang/freeboard/domain/FreeBoardDto.java
@@ -0,0 +1,47 @@
+package com.semi.dangjang.freeboard.domain;
+
+import com.semi.dangjang.common.BaseDto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor(access=AccessLevel.PUBLIC)  //기본생성자를 만들어준다 
+public class FreeBoardDto extends BaseDto{
+	    
+	private String free_seq="";
+	private String user_id="";
+	private String title="";
+	private String content="";
+	private String image="";
+	private String wdate="";
+	private String like_cnt="";
+	private String hit="";
+	private String user_seq="";
+	private String filename="";
+	private String image_url="";
+	
+	@Builder
+	public FreeBoardDto(String free_seq, String user_id, String title, String content, String image, String wdate,
+			String like_cnt, String hit, String user_seq) {
+		super();
+		this.free_seq = free_seq;
+		this.user_id = user_id;
+		this.title = title;
+		this.content = content;
+		this.image = image;
+		this.wdate = wdate;
+		this.like_cnt = like_cnt;
+		this.hit = hit;
+		this.user_seq = user_seq;
+		this.filename = filename;
+		this.image_url = image_url;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/semi/dangjang/freeboard/service/FreeBoardServiceImpl.java
@@ -0,0 +1,87 @@
+package com.semi.dangjang.freeboard.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.semi.dangjang.comment.domain.FBCommentDto;
+import com.semi.dangjang.comment.repository.FBCommentDao;
+import com.semi.dangjang.freeboard.domain.FreeBoardDto;
+import com.semi.dangjang.freeboard.repository.FreeBoardDao;
+
+
+
+
+@Service("freeboardService")
+public class FreeBoardServiceImpl implements FreeBoardService{
+
+	@Resource(name="freeboardDao")
+	FreeBoardDao dao;
+	
+	@Resource(name="fbcommentDao")
+	FBCommentDao fbcommentDao;
+
+	@Override
+	public List<FreeBoardDto> getList(FreeBoardDto dto) {
+		
+		return dao.getList(dto);
+	}
+
+	@Override
+	public FreeBoardDto getView(String user_id) {
+		
+		return dao.getView(user_id);
+	}
+
+	@Override
+	public void insert(FreeBoardDto dto) {
+		dao.insert(dto);
+		
+	}
+
+	@Override
+	public int getTotalCnt(FreeBoardDto dto) {
+		
+		return dao.getTotalCnt(dto);
+	}
+
+	@Override
+	public void update(FreeBoardDto dto) {
+		dao.update(dto);
+		
+	}
+
+	@Override
+	public void delete(FreeBoardDto dto) {
+		dao.delete(dto);
+		
+	}
+
+	@Override
+	public void fbcommentinsert(FBCommentDto dto) {
+		fbcommentDao.fbcommentinsert(dto);
+		
+	}
+
+	@Override
+	public List<FBCommentDto> fbcommentList(FBCommentDto dto) {
+		// TODO Auto-generated method stub
+		return fbcommentDao.fbcommentList(dto);
+	}
+
+	@Override
+	public int fbcommentTotal(FBCommentDto dto) {
+		return fbcommentDao.fbcommentTotal(dto);
+		
+	}
+	
+	
+}
+
+
+
+
+
+
